c4889028454647f32d0ee62d6ed38a532ab50611
MyFRA <tomyntapss@gmail.com>
2026-02-17 20:36:45 +0700

    35-search-insert-position

--- typescript/35-search-insert-position.ts
+++ typescript/35-search-insert-position.ts
@@ -4,34 +4,23 @@ function searchInsert(nums: number[], target: number): number {
 
     while (left < right) {
         const mid = Math.floor(left + (right - left) / 2);
-        if (nums[mid] == target) {
+
+        if (nums[mid] === target) {
             return mid;
         }
 
         if (nums[mid] < target) {
-            left = mid;
+            left = mid + 1;
         } else {
             right = mid;
         }
-
-        if (right - left == 1) {
-            if (target < nums[0]) {
-                return 0;
-            } else if (target > nums[nums.length - 1]) {
-                return nums.length;
-            } else {
-                return left + 1;
-            }
-        }
     }
 
-    if (target < nums[0]) {
-        return 0;
-    } else if (target > nums[nums.length - 1]) {
-        return nums.length;
-    } else {
-        return nums.length - 1;
+    if (nums[left] < target) {
+        return left + 1;
     }
+
+    return left;
 }
 
-console.log(searchInsert([1, 3, 5], 1));
+console.log(searchInsert([1, 3, 5, 6], 2));
